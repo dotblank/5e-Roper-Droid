0e46ad85e76ee37686688d012ac1480414cd14b5
Matthew Balch <matbalch@vt.edu>
2015-06-26 11:51:52 -0400

    Added files auto-generated by android tool project creation process

---
+++ src/com/github/harverst/roper/Roper5eDroid.java
@@ -0,0 +1,15 @@
+package com.github.harverst.roper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Roper5eDroid extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
